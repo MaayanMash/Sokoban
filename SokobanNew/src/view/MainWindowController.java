be2f3febff9bd5479fe59a1ec0fd645b0215c292
maayan <maayan.mash@gmail.com>
2017-01-24 12:01:56 +0200

    exit

--- SokobanNew/src/view/MainWindowController.java
+++ SokobanNew/src/view/MainWindowController.java
@@ -5,10 +5,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
+import javax.print.attribute.standard.Media;
 import commons.Level2D;
 import java.util.Observable;
 import java.util.Optional;
-
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -23,11 +23,12 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Popup;
-import javafx.stage.PopupBuilder;
+import javafx.scene.media.MediaPlayer;
 
 public class MainWindowController extends Observable implements Initializable, iView {
 	
@@ -42,44 +43,16 @@ public class MainWindowController extends Observable implements Initializable, i
 	//steps
 	@FXML private Text SokobanSteps;
 	
+	//music
+	private String musicString;
+	private MediaPlayer player;
+	private boolean music;
+	
 	public MainWindowController() {
 		this.CounterTime=new SimpleStringProperty();
-	}
-	
-	@Override
-	public void createBindSteps(StringProperty Counter){
-		this.SokobanSteps.textProperty().bind(Counter);
-	}
-	
-	public void startTimer() {
-		this.countTime=0;
-		this.timer=new Timer();
-		SokobanTimer.textProperty().bind(this.CounterTime);
-		this.timer.scheduleAtFixedRate(new TimerTask() {
-			
-			@Override
-			public void run() {
-				CounterTime.set(" "+(++countTime));
-			}
-		}, 0, 1000);
-		
-	}
-	
-	public void stopTimer(){
-		if(this.timer!=null)
-			this.timer.cancel();
-	}
-	
-	public void continueTime(){
-		this.timer=new Timer();
-		SokobanTimer.textProperty().bind(this.CounterTime);
-		this.timer.scheduleAtFixedRate(new TimerTask() {
-			
-			@Override
-			public void run() {
-				CounterTime.set(" "+(++countTime));
-			}
-		}, countTime, 1000);
+		this.musicString=new File("./resources/music/Game Music.mp3").toURI().toString();
+		this.player=new MediaPlayer(new javafx.scene.media.Media(musicString));
+		this.music=false;
 	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -104,33 +77,9 @@ public class MainWindowController extends Observable implements Initializable, i
 			}
 		});
 	}
-	
-	public void openFile(){
-		FileChooser fc =new FileChooser();
-		fc.setTitle("open level");
-		fc.setInitialDirectory(new File("./resources"));
-		fc.getExtensionFilters().addAll(new ExtensionFilter("Text File", "*.txt"), new ExtensionFilter("XML File", "*.xml"), new ExtensionFilter("Object File", "*.obj"));
-		
-		File chosen = fc.showOpenDialog(null);
-		if(chosen != null){
-			setChanged();
-			notifyObservers("load "+ chosen.getPath());
-			setFocus();
-			stopTimer();
-			startTimer();
-		}
-	}
-	public void saveFile(){
-		FileChooser fc =new FileChooser();
-		fc.setTitle("save level");
-		fc.setInitialDirectory(new File("./resources"));
-		fc.getExtensionFilters().addAll(new ExtensionFilter("Text File", "*.txt"), new ExtensionFilter("XML File", "*.xml"), new ExtensionFilter("Object File", "*.obj"));
-		
-		File chosen = fc.showSaveDialog(null);
-		if(chosen != null){
-			setChanged();
-			notifyObservers("save " + chosen.getPath());
-		}
+	@Override
+	public void createBindSteps(StringProperty Counter){
+		this.SokobanSteps.textProperty().bind(Counter);
 	}
 	private void setFocus()
 	{
@@ -149,19 +98,6 @@ public class MainWindowController extends Observable implements Initializable, i
             }
         });
 	}
-
-	@Override
-	public void displayLevel(Level2D theLevel) {
-		this.SokobanDisplayer.setSokobanCol(theLevel.getSizeCol());
-		this.SokobanDisplayer.setSokobanRow(theLevel.getSizeRow());
-		this.SokobanDisplayer.setSokobanData(theLevel.getBoared());
-		//need to change position to twopoint
-		//this.SokobanDisplayer.setcCol(theLevel.getPosCharacter().getY());
-		//this.SokobanDisplayer.setcRow(theLevel.getPosCharacter().getX());
-		
-		ifsolved(theLevel);
-	}
-
 	public void ifsolved(Level2D theLevel){
 		if (theLevel.ifSolved())
 			Platform.runLater(new Runnable() {
@@ -176,9 +112,92 @@ public class MainWindowController extends Observable implements Initializable, i
 			});
 	}
 	
+	public void startTimer() {
+		this.countTime=0;
+		this.timer=new Timer();
+		SokobanTimer.textProperty().bind(this.CounterTime);
+		this.timer.scheduleAtFixedRate(new TimerTask() {
+			
+			@Override
+			public void run() {
+				CounterTime.set(" "+(++countTime));
+			}
+		}, 0, 1000);
+		
+	}
+	public void stopTimer(){
+		if(this.timer!=null)
+			this.timer.cancel();
+	}
+	public void continueTime(){
+		this.timer=new Timer();
+		SokobanTimer.textProperty().bind(this.CounterTime);
+		this.timer.scheduleAtFixedRate(new TimerTask() {
+			
+			@Override
+			public void run() {
+				CounterTime.set(" "+(++countTime));
+			}
+		}, countTime, 1000);
+	}
+
+	public void startStopMusic(){
+		if (this.music)
+		{
+			this.player.pause();
+			this.music=false;
+		}
+		else
+		{
+			this.player.play();
+			this.music=true;
+		}
+	}
+
+	public void openFile(){
+		FileChooser fc =new FileChooser();
+		fc.setTitle("open level");
+		fc.setInitialDirectory(new File("./resources"));
+		fc.getExtensionFilters().addAll(new ExtensionFilter("Text File", "*.txt"), new ExtensionFilter("XML File", "*.xml"), new ExtensionFilter("Object File", "*.obj"));
+		
+		File chosen = fc.showOpenDialog(null);
+		if(chosen != null){
+			setChanged();
+			notifyObservers("load "+ chosen.getPath());
+			setFocus();
+			stopTimer();
+			startTimer();
+			startStopMusic();
+		}
+	}
+	public void saveFile(){
+		FileChooser fc =new FileChooser();
+		fc.setTitle("save level");
+		fc.setInitialDirectory(new File("./resources"));
+		fc.getExtensionFilters().addAll(new ExtensionFilter("Text File", "*.txt"), new ExtensionFilter("XML File", "*.xml"), new ExtensionFilter("Object File", "*.obj"));
+		
+		File chosen = fc.showSaveDialog(null);
+		if(chosen != null){
+			setChanged();
+			notifyObservers("save " + chosen.getPath());
+		}
+	}
+	@Override
+	public void displayLevel(Level2D theLevel) {
+		this.SokobanDisplayer.setSokobanCol(theLevel.getSizeCol());
+		this.SokobanDisplayer.setSokobanRow(theLevel.getSizeRow());
+		this.SokobanDisplayer.setSokobanData(theLevel.getBoared());
+		//need to change position to twopoint
+		//this.SokobanDisplayer.setcCol(theLevel.getPosCharacter().getY());
+		//this.SokobanDisplayer.setcRow(theLevel.getPosCharacter().getX());
+		
+		ifsolved(theLevel);
+	}
 	@Override
 	public void displayExit() {
 		stopTimer();
+		if (this.music)
+			startStopMusic();
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Exit");
 		alert.setHeaderText("Are you sure you want to exit?");
@@ -187,16 +206,12 @@ public class MainWindowController extends Observable implements Initializable, i
 		if (result.get() == ButtonType.OK){
 		    setChanged();
 		    notifyObservers("exit");
+		    Platform.exit();
 		} else {
 			continueTime();
 		    alert.close();
-		    Platform.exit();
 		}
 	}
-	public StringProperty getCounter() {
-		return CounterTime;
-	}
-
 	@Override
 	public void displayMassege(String massege) {
 		Platform.runLater(new Runnable() {
